4f1d95deeabba8a18606fa2aa93e2190923c8215
ednayssell7 <ednayrr@gmail.com>
2020-11-28 12:42:15 -0700

    using DictReader for csv file

---
+++ 08_lector_dos_csv.py
@@ -0,0 +1,14 @@
+
+#utilizando modulo csv para lectura de archivos csv
+import csv
+
+filename = 'employees.csv'
+
+with open(filename, mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        #print(row)
+        #print(row['Name'])
+        s = row.get('Salary')
+        n = row.get('Name')
+        print(f'{n} earns {s}')
