4ea69465b2edddbcbd60e6903890c0d864d8b071
ednayssell7 <ednayrr@gmail.com>
2020-11-28 09:48:52 -0700

    tarea

---
+++ 02_pozoleria_descuento.py
@@ -0,0 +1,7 @@
+#tarea aplicar conocimientos de uso de la funcion map en python
+
+#agregar un descueno aleatorio de entre 5, 10 y 15 % a todos los precios
+#imprimir menu sin descuento
+#imprimir menu con descuento
+#usar random 
+precios_en_menu = [1975, 1825, 275, 1500, 850, 675, 1175, 1600, 2175, 2175, 625, 1150, 1175, 350, 1125, 1250, 1875, 1275, 825, 1575, 300, 1275, 875, 1650, 875]
---
+++ 03_numeros_magicos.py
@@ -0,0 +1,6 @@
+#aplicar conocimientos de la funcion map en python
+
+#solicitar al usuario un numero menor que 10 y mayor que 0
+#utilizando el numero del usuario aplicar a la lista a todos los elementos los elementos de la lista una multiplicacion del mismo numero del usuario
+#convertir los numeros a enteros
+#convertir numeros pares a true
