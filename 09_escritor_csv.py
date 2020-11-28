c16a3583432cde75ae3a36b0aafd27b8826e315b
ednayssell7 <ednayrr@gmail.com>
2020-11-28 13:14:03 -0700

    Homework

--- 09_escritor_csv.py
+++ 09_escritor_csv.py
@@ -6,6 +6,17 @@ filename = 'tmp/archivo_fake.csv'
 #especificar columnas
 colums = ['id', 'name', 'age']
 
+#TAREA GENERAR UN LISTADO DE IDS USANDO RANDOM Y CADA FUNCION DEBE DE RETORNAR LOS IDS FALSOS
+
+def fake_id():
+    pass 
+
+def fake_name():
+    pass
+
+def fake_age():
+    pass
+
 #abrir contexto
 with open(filename, mode='w', newline='') as csv_file:
     #crear una variable para escritor
@@ -16,8 +27,8 @@ with open(filename, mode='w', newline='') as csv_file:
     counter = 1
     while counter <= 20:
         writer.writerow({
-            'id': counter * 123,
-            'name': f'Dalai {counter} {counter * 123}',
-            'age': f'{counter * 5}'
+            'id': fake_id(),
+            'name': fake_name(),
+            'age': fake_age()
         })
         counter += 1
