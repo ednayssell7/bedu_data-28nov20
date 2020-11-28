893b3bdcbe2e0e516458c42f8caf99594fbe3eaf
ednayssell7 <ednayrr@gmail.com>
2020-11-28 12:45:34 -0700

    added comments for dictionary .get

--- 08_lector_dos_csv.py
+++ 08_lector_dos_csv.py
@@ -5,10 +5,12 @@ import csv
 filename = 'employees.csv'
 
 with open(filename, mode='r') as csv_file:
+    #csv file converter into a dictionari
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
         #print(row)
-        #print(row['Name'])
+        #print(row['Name']) not ideal
+        #every dictionary has .get method
         s = row.get('Salary')
         n = row.get('Name')
         print(f'{n} earns {s}')
