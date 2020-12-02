6a783fe3dec89730325cbb4b2d46dfd3c950dc51
ednayssell7 <ednayrr@gmail.com>
2020-12-02 01:01:44 -0700

    added comments

--- 03_numeros_magicos.py
+++ 03_numeros_magicos.py
@@ -1,10 +1,4 @@
 #aplicar conocimientos de la funcion map en python
-
-
-#utilizando el numero del usuario aplicar a la lista a todos los elementos los elementos de la lista una multiplicacion del mismo numero del usuario
-
-#convertir numeros pares a true
-
 import random
 
 Numeros = ['3', '148', '74', '71', '4', '83', '95', '20', '61', '10', '69', '67', '23', '164', '97', '67', '144', '200', '38', '90', '200', '162', '6', '180', '65', '71', '90', '182', '16', '132', '182', '108', '90', '196', '48', '2', '158', '88', '39', '39', '54', '80', '89', '3', '90', '170', '88', '71', '142', '45', '81', '194', '36', '39', '30', '33', '38', '44', '134', '43', '12', '52', '170', '162', '192', '83', '18', '176', '120', '28', '86', '188', '51', '11', '96', '13', '198', '34', '66', '23', '200', '62', '194', '91', '51', '26', '152', '186', '86', '38', '46', '66', '83', '66', '40', '2', '20', '12', '91', '53']
@@ -22,6 +16,7 @@ def multiplicacion(numero):
     num_multi = numero * numero_random
     return num_multi
 
+#convertir numeros pares a true
 def num_pares(num_par):
     if num_par % 2 == 0:
         return True   
