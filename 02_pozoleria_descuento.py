5a37dd66f8cc56ea20148ea583017106be5c9169
ednayssell7 <ednayrr@gmail.com>
2020-12-02 00:54:04 -0700

    added function to multiply output number to every element on a list and par number using map

--- 02_pozoleria_descuento.py
+++ 02_pozoleria_descuento.py
@@ -4,4 +4,38 @@
 #imprimir menu sin descuento
 #imprimir menu con descuento
 #usar random 
+import random
+
 precios_en_menu = [1975, 1825, 275, 1500, 850, 675, 1175, 1600, 2175, 2175, 625, 1150, 1175, 350, 1125, 1250, 1875, 1275, 825, 1575, 300, 1275, 875, 1650, 875]
+
+
+descuento = [5, 10, 15]
+descuento_aleatorio = random.choices(descuento)
+dividendo = 100
+        
+def operacion_desc(precio):
+    for j in descuento:
+        for i in descuento_aleatorio:
+            desc_random = i
+            descuento_total = precio * (desc_random/dividendo)
+            descuento_total = float(descuento_total)
+        total = precio - descuento_total
+        total = float(total)
+        return desc_random
+
+#def operacion_total(precio_1):
+    #for j in descuento_total:
+       # descuento_total = random.sample(descuento, 1)
+      #  Total = precio_1 - descuento_total
+     #   Total = float(Total)
+    #return Total
+
+ 
+aleatorio_desc_precios = list(map(operacion_desc, precios_en_menu))    
+desc_precios_menu = list(map(operacion_desc, precios_en_menu))
+
+print(f'Precios del menu: {precios_en_menu}')
+print(f'Precios con descuento: {desc_precios_menu}')
+
+
+
--- 03_numeros_magicos.py
+++ 03_numeros_magicos.py
@@ -1,6 +1,36 @@
 #aplicar conocimientos de la funcion map en python
 
-#solicitar al usuario un numero menor que 10 y mayor que 0
+
 #utilizando el numero del usuario aplicar a la lista a todos los elementos los elementos de la lista una multiplicacion del mismo numero del usuario
-#convertir los numeros a enteros
+
 #convertir numeros pares a true
+
+import random
+
+Numeros = ['3', '148', '74', '71', '4', '83', '95', '20', '61', '10', '69', '67', '23', '164', '97', '67', '144', '200', '38', '90', '200', '162', '6', '180', '65', '71', '90', '182', '16', '132', '182', '108', '90', '196', '48', '2', '158', '88', '39', '39', '54', '80', '89', '3', '90', '170', '88', '71', '142', '45', '81', '194', '36', '39', '30', '33', '38', '44', '134', '43', '12', '52', '170', '162', '192', '83', '18', '176', '120', '28', '86', '188', '51', '11', '96', '13', '198', '34', '66', '23', '200', '62', '194', '91', '51', '26', '152', '186', '86', '38', '46', '66', '83', '66', '40', '2', '20', '12', '91', '53']
+Numeros_int =[int(x) for x in Numeros]
+print(type(Numeros_int))
+
+
+#solicitar al usuario un numero menor que 10 y mayor que 0
+
+numero_random = input(f'Hola, selecciona un numero del 1 al 10: ')
+numero_random = int(numero_random) #convertir los numeros a entero   
+
+def multiplicacion(numero):
+    # Utilizando el numero del usuario, aplicar a todos los elementos de la lista una multiplicacion por el numero del usuario
+    num_multi = numero * numero_random
+    return num_multi
+
+def num_pares(num_par):
+    if num_par % 2 == 0:
+        return True   
+        
+multiplicacion_lista = list(map(multiplicacion, Numeros_int))
+pares = list(map(num_pares, Numeros_int))
+print(f'Multiplicacion de {numero_random} por cada numero de la lista: {multiplicacion_lista}')
+print(f'Cada True es un numero par: {pares}')
+        
+
+    
+
