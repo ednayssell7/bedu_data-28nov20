8bd58f845defc14fdb6f1874589e59952c47f43a
ednayssell7 <ednayrr@gmail.com>
2020-12-04 00:38:12 -0700

    still missing diferent results on every row

--- 10_tarea_escritor_csv.py
+++ 10_tarea_escritor_csv.py
@@ -9,25 +9,30 @@ colums = ['id', 'name', 'age']
 
 #TAREA GENERAR UN LISTADO DE IDS USANDO RANDOM Y CADA FUNCION DEBE DE RETORNAR LOS IDS FALSOS
 
-id_fake = [123, 456, 789, 101, 321, 654, 987, 111, 999, 222]
-names_fake = ['Edna', 'Angel', 'Maria', 'Leticia', 'Denisse', 'Jose', 'Leonardo', 'Samuel', 'Hugo', 'Julieta']
-age_fakes = [12, 28, 70, 4, 2, 35, 50, 30, 27, 59]
+id_fake_ = [123, 456, 789, 101, 321, 654, 987, 111, 999, 222]
+name_fake = ['Edna', 'Angel', 'Maria', 'Leticia', 'Denisse', 'Jose', 'Leonardo', 'Samuel', 'Hugo', 'Julieta']
+age_fake = [12, 28, 70, 4, 2, 35, 50, 30, 27, 59]
 
 
-def fake_id(id_fake):
-    id_fake = random.choices(id_fake)
-    return id_fake
+def fake_id(idfake):
+    random_id = random.choice(id_fake_)
+    return random_id
 
-def fake_name(names_fake):
-    names_fake = random.choices(names_fake)
-    return names_fake
+def fake_name(namesfake):
+    random_names = random.choice(name_fake)
+    return random_names
 
-def fake_age(age_fake):
-    age_fake = random.choices(age_fake)
-    return age_fake 
+def fake_age(agefake):
+    random_age= random.choice(age_fake)
+    return random_age
+
+randomID = random.choice(list(map(fake_id, id_fake_)))
+randomName = random.choice(list(map(fake_name, name_fake)))
+randomAge = random.choice(list(map(fake_age, age_fake)))
 
 #abrir contexto
 with open(filename, mode='w', newline='') as csv_file:
+    #writer = csv.DicWriter(file, delimiter='|')
     #crear una variable para escritor
     writer = csv.DictWriter(csv_file, fieldnames=colums)
 
@@ -36,8 +41,8 @@ with open(filename, mode='w', newline='') as csv_file:
     counter = 1
     while counter <= 10:
         writer.writerow({
-            'id': fake_id(),
-            'name': fake_name(),
-            'age': fake_age()
+            'id': randomID,
+            'name': randomName,  
+            'age': randomAge
         })
         counter += 1
