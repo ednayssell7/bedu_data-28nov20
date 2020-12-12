3cf7026f86d386428482c4d4ddeb71bcb544f839
ednayssell7 <ednayrr@gmail.com>
2020-12-12 12:20:14 -0700

    update done! gives back a random list

--- 10_tarea_escritor_csv.py
+++ 10_tarea_escritor_csv.py
@@ -14,21 +14,14 @@ name_fake = ['Edna', 'Angel', 'Maria', 'Leticia', 'Denisse', 'Jose', 'Leonardo',
 age_fake = [12, 28, 70, 4, 2, 35, 50, 30, 27, 59]
 
 
-def fake_id(idfake):
-    random_id = random.choice(id_fake_)
-    return random_id
+def fake_id():
+    return random.choice(id_fake_)
 
-def fake_name(namesfake):
-    random_names = random.choice(name_fake)
-    return random_names
+def fake_name():
+    return random.choice(name_fake)
 
-def fake_age(agefake):
-    random_age= random.choice(age_fake)
-    return random_age
-
-randomID = random.choice(list(map(fake_id, id_fake_)))
-randomName = random.choice(list(map(fake_name, name_fake)))
-randomAge = random.choice(list(map(fake_age, age_fake)))
+def fake_age():
+    return random.choice(age_fake)
 
 #abrir contexto
 with open(filename, mode='w', newline='') as csv_file:
@@ -41,8 +34,8 @@ with open(filename, mode='w', newline='') as csv_file:
     counter = 1
     while counter <= 10:
         writer.writerow({
-            'id': randomID,
-            'name': randomName,  
-            'age': randomAge
+            'id': fake_id(),
+            'name': fake_name(),  
+            'age': fake_age()
         })
         counter += 1
